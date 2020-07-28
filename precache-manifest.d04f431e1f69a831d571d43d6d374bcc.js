02cc6c5db0374d8163e965a94139c79c0e196685
unknown <plocieniak@interia.pl>
2020-07-28 16:52:49 +0200

    Updates

---
+++ precache-manifest.d04f431e1f69a831d571d43d6d374bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85744f52d87c0a5f372d698967de7c4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88f102bf6cd4ac073103",
+    "url": "/monsters-rolodex/static/css/main.0194d6fa.chunk.css"
+  },
+  {
+    "revision": "5b13380b601cb1a662fc",
+    "url": "/monsters-rolodex/static/js/2.7740e645.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7740e645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f102bf6cd4ac073103",
+    "url": "/monsters-rolodex/static/js/main.e2540c6e.chunk.js"
+  },
+  {
+    "revision": "20a292f8e6cf455daa18",
+    "url": "/monsters-rolodex/static/js/runtime-main.1b499626.js"
+  }
+]);
